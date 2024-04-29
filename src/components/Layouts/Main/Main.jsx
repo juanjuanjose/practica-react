b9da756c5cedb3460cb3d5584ca1431469276c11
juanjose <juanjoperez2121@gmail.com>
2024-04-29 11:49:03 -0500

    tercera clase

--- src/components/Layouts/Main/Main.jsx
+++ src/components/Layouts/Main/Main.jsx
@@ -6,15 +6,15 @@ const Main = () => {
   return (
     <main className='main'>
 
-      <PortfolioSection title="titulo 1" hasBorder={true} progressValue="70">
+      <PortfolioSection title="titulo 1" hasBorder={true} >
         <p>esto tambien funciona</p>
       </PortfolioSection>
 
-      <PortfolioSection title="titulo 2" hasBorder={false} progressValue="30">
+      <PortfolioSection title="titulo 2" hasBorder={false} >
         content 2
       </PortfolioSection>
 
-      <PortfolioSection title="titulo 3" hasBorder={true} progressValue="10">
+      <PortfolioSection title="titulo 3" hasBorder={true} >
         content 3
       </PortfolioSection>
       <PortfolioSection title="titulo 4" hasBorder={false} progressValue="95">
